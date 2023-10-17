0af997971ac553ae68efd7198fb8c0ce89a42c5a
Srt-Jubayer <147469746+Srt-Jubayer@users.noreply.github.com>
2023-10-17 10:37:36 +0600

    Pernonal Protfolio
    Pernonal Protfolio design

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text",{
+    strings:["Fronted Developer", "UI/UX Designer", "App Designer"],
+    typeSpeed: 100,
+    BackSpeed: 100,
+    BackDeley: 1000,
+    loop: true
+
+})
